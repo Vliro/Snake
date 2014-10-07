f8617187dfd462e4d62254fe07651cb5a72b023c
Albert Sjölund <albertsjolund@hotmail.com>
2014-10-07 22:54:24 +0200

    Fix circlist

--- SDL_Snake/include/CircList.h
+++ SDL_Snake/include/CircList.h
@@ -7,37 +7,55 @@ class CircList
     public:
         CircList() {tail = new Node();}
         CircList(T * segment);
-        virtual ~CircList() {delete tail;}
+        virtual ~CircList();
         void add(T * segment);
+        T & gettail() {return *(tail->value);}
+        int count;
     protected:
     private:
         struct Node {
             T * value;
-            T * next;
-            T * previous;
+            Node * next;
+            Node * previous;
         };
         Node * tail;
-        T * gettail();
 };
 template<typename T>
 CircList<T>::CircList(T * segment)
 {
     tail = new Node();
     tail->value = segment;
-    tail->next = segment;
-    tail->previous = segment;
+    tail->next = tail;
+    tail->previous = tail;
+    count = 1;
 }
 
 template<typename T>
 void  CircList<T>::add(T * segment)
 {
-    std::cout << "Segment: " << segment;
     Node * newnode = new Node();
     newnode->next = tail->next;
-    newnode->previous = tail->value;
+    newnode->previous = tail;
     newnode->value = segment;
-    tail->next = newnode->value;
+    tail->next = newnode;
     tail = newnode;
-    std::cout << std::endl << "New tail.value : " << tail->value << std::endl;
+    count++;
+}
+
+template<typename T>
+CircList<T>::~CircList()
+{
+    Node * temp = new Node();
+    if(tail == NULL || temp == NULL) return;
+    temp->next = tail->next;
+    delete tail;
+    for(int i = count; i > 1; i-- )
+    {
+        temp->previous = temp->next;
+        temp->next = temp->previous->next;
+        temp->next = temp->previous->next;
+        delete temp->previous;
+    }
+    delete temp;
 }
 #endif // CIRCLIST_H
