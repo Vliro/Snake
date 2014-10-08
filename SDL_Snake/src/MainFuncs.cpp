8a5015299f6d4d8d0facbcae127c7d9831a3dc8a
Albert Sjölund <albertsjolund@hotmail.com>
2014-10-08 19:47:35 +0200

    Added keyboard handling. Added function for creating new snakepart, based on the keyboard event.
    TODO:
    Looping through circlist, proper handling of global variables.
    Rendering the snakes through gameloop.
    Gameloop.
    Food and all related functions.

--- SDL_Snake/src/MainFuncs.cpp
+++ SDL_Snake/src/MainFuncs.cpp
@@ -7,15 +7,25 @@
 bool StartSnake()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Init(SDL_INIT_TIMER);    
+    SDL_Init(SDL_INIT_TIMER);
     mainwindow = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
     snakepart * head = new snakepart();
     *head = constants::center;
-    CircList<snakepart> snake(head);
+    CircList<snakepart> * snake = new CircList<snakepart>(head);
     return 0;
 }
 
 SDL_Window * GetMainWindow()
 {
     return mainwindow;
+}
+
+CircList<snakepart> * GetSnake()
+{
+    return snake;
+}
+
+void MainGameLoop()
+{
+
 }
--- SDL_Snake/src/main.cpp
+++ SDL_Snake/src/main.cpp
@@ -1,19 +1,20 @@
 #include <SDL2/SDL.h>
 #include "MainFuncs.h"
-
+#include "EventHandler.h"
+#include "Maths.h"
 
 int main ( int argc, char** argv )
 {
     StartSnake();
     SDL_Surface * surface = SDL_GetWindowSurface(GetMainWindow());
-    SDL_Rect b;
-    b.h = 16;
-    b.w = 12;
-    b.x = 100;
-    b.y = 100;
-    SDL_FillRect(surface, &b, SDL_MapRGB(surface->format, 220, 220, 220));
+    snakepart b = constants::center;
+    SDL_FillRect(surface, PartToRect(&b), SDL_MapRGB(surface->format, 220, 220, 220));
     SDL_UpdateWindowSurface(GetMainWindow());
-    
-    SDL_Delay(10000);
+    SDL_Delay(3000);
+    keyboardloop();
+    SDL_UpdateWindowSurface(GetMainWindow());
+    SDL_Delay(3000);
+    SDL_DestroyWindow(GetMainWindow());
+    SDL_Quit();
 }
 
